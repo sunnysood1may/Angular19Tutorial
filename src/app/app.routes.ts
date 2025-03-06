e5c1173b9124f5e3e438767fe02be154d580b208
sunny sood <sunny1may@gmail.com>
2025-03-06 16:04:43 +0530

    Angular Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FirstComponent } from './first/first.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: FirstComponent
+    },
+    {
+        path: 'about', component: AboutComponent
+    },
+    {
+        path: 'contact', component: ContactComponent
+    }
+];
