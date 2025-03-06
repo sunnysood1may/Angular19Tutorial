57a4e5b842493269215b78254fe7127270f6e019
sunny sood <sunny1may@gmail.com>
2025-03-06 12:51:27 +0530

    Components in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './first/first.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/first/first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  imports: [],
+  templateUrl: './first.component.html',
+  styleUrl: './first.component.css'
+})
+export class FirstComponent {
+  title = "First Component Load";
+
+}
