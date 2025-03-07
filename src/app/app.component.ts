09cf6dba68a2e1b9d8cb4a165c1542ba83ae45b9
sunny sood <sunny1may@gmail.com>
2025-03-07 10:17:39 +0530

    navbar header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FirstComponent } from './first/first.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
